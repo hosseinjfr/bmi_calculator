0c8c584d79eb2ed26fe902e039e509f6b7aeb88e
hossein jafarinejad <hosseinjn@outlook.com>
2021-12-31 22:31:17 +0330

    add bottom container and const value

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,5 +1,13 @@
 import 'package:flutter/material.dart';
 
+const bottomContainerHeight = 50.0;
+const activeCardColor = Color(0xFF1D1E33);
+const bottomContainerColor = Color(0xFFEB1555);
+const cardMargin = EdgeInsets.all(15.0);
+const bottomContainerMargin = EdgeInsets.only(top: 10.0);
+
+
+
 class InputPage extends StatefulWidget {
   const InputPage({Key? key}) : super(key: key);
 
@@ -22,28 +30,34 @@ class _InputPageState extends State<InputPage> {
               child: Row(
                 children: [
                   Expanded(
-                    child: ReusableContainer(colour: Color(0xFF1D1E33)),
+                    child: ReusableContainer(colour: activeCardColor),
                   ),
                   Expanded(
-                    child: ReusableContainer(colour: Color(0xFF1D1E33)),
+                    child: ReusableContainer(colour: activeCardColor),
                   ),
                 ],
               ),
             ),
             Expanded(
-              child: ReusableContainer(colour: Color(0xFF1D1E33)),
+              child: ReusableContainer(colour: activeCardColor),
             ),
             Expanded(
               child: Row(
                 children: [
                   Expanded(
-                    child: ReusableContainer(colour: Color(0xFF1D1E33)),
+                    child: ReusableContainer(colour: activeCardColor),
                   ),
                   Expanded(
-                    child: ReusableContainer(colour: Color(0xFF1D1E33)),
+                    child: ReusableContainer(colour: activeCardColor),
                   ),
                 ],
               ),
+            ),
+            Container(
+              color: bottomContainerColor,
+              margin: bottomContainerMargin,
+              width: double.infinity,
+              height: bottomContainerHeight,
             ),
           ],
         ),
@@ -63,7 +77,7 @@ class ReusableContainer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.all(15.0),
+      margin: cardMargin,
       decoration: BoxDecoration(
         color: colour,
         borderRadius: BorderRadius.circular(10.0),
